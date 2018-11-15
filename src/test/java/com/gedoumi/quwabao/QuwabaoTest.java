194b465a1a8e58f5753cf0631e9bf4256841f603
snownersd <snownersd@126.com>
2018-11-15 10:28:49 +0800

    新版本

--- src/test/java/com/gedoumi/quwabao/QuwabaoTest.java
+++ src/test/java/com/gedoumi/quwabao/QuwabaoTest.java
@@ -1,14 +1,29 @@
 package com.gedoumi.quwabao;
 
-import com.gedoumi.quwabao.common.utils.CodeUtils;
 import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class QuwabaoTest {
 
     @Test
-    public void test1() {
-        String s = CodeUtils.generateCode();
-        System.out.println(s);
+    public void test1() throws ParseException {
+        LocalDateTime now = LocalDateTime.now();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = sdf.parse("2018-10-05 12:04:25");
+        LocalDateTime localDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        Duration between = Duration.between(localDateTime, now);
+        System.out.println("" + between.toNanos());
+        System.out.println("Millis:" + between.toMillis());
+        System.out.println("Minutes:" + between.toMinutes());
+        System.out.println("Days:" + between.toDays());
     }
 
 }
